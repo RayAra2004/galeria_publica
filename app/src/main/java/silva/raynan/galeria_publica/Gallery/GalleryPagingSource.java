48919031fa81d18d131dc97bfb02739992633451
RayAra2004 <raynan.araujo@gmail.com>
2023-08-25 12:53:28 -0300

    Página 19 --> Passo 8.3

--- app/src/main/java/silva/raynan/galeria_publica/Gallery/GalleryPagingSource.java
+++ app/src/main/java/silva/raynan/galeria_publica/Gallery/GalleryPagingSource.java
@@ -1,4 +1,24 @@
 package silva.raynan.galeria_publica.Gallery;
 
-public class GalleryPagingSource {
+import androidx.annotation.NonNull;
+import androidx.paging.ListenableFuturePagingSource;
+import androidx.paging.PagingState;
+
+import javax.annotation.Nullable;
+
+public class GalleryPagingSource extends ListenableFuturePagingSource<Integer, ImageData> {
+    GalleryRepository galleryRepository;
+    Integer initialLoadSize = 0;
+
+    public GalleryPagingSource(GalleryRepository galleryRepository){
+        this.galleryRepository = galleryRepository;
+    }
+
+    @Nullable
+    @Override
+    public Integer getRefreshKey(@NonNull PagingState<Integer, ImageData> pagingState){
+        return null;
+    }
+
+
 }
